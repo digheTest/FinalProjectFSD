f06359bca09cfd7b8f4b60ad992a8fb823faf912
digheTest <52630126+digheTest@users.noreply.github.com>
2019-09-03 10:35:52 +0530

    Testing changes 3

--- task-mgr/src/app/utils/constants.ts
+++ task-mgr/src/app/utils/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
   static UPDATE = "Update";
   static ADD = "Add";
-  static BASE_URL = "http://localhost:58376/api/values";
+  static BASE_URL = "http://localhost:8095/api/values";
 }
