fc11bdbe96b826864b0652259e0c9c6e305415cd
digheTest <52630126+digheTest@users.noreply.github.com>
2019-09-03 10:28:10 +0530

    Testing changes 2

--- task-mgr/src/app/services/task.service.ts
+++ task-mgr/src/app/services/task.service.ts
@@ -11,7 +11,7 @@ import { of } from "rxjs";
   providedIn: "root"
 })
 export class TaskService {
-  static BASE_URL = "https://localhost:8095/api/values";
+  static BASE_URL = "http://localhost:8095/api/values";
 
   tasks: Array<Task> = [];
   parentTasks: Array<ParentTask> = [];
